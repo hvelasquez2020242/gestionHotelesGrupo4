358633c9fd4be8586f3d1d199423178a6904d1a1
hugo <hvelasquez-2020242@kinal.edu.gt>
2022-07-01 00:28:34 -0600

    Reservacion

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const HotelRutas = require('./src/routes/hotel.routes');
 const HabitacionRutas = require('./src/routes/habitacion.routes');
 const EventoRutas = require('./src/routes/evento.routes');
 const HospedadosRutas = require('./src/routes/hospedados.routes')
+const ReservacionRutas = require('./src/routes/reservacion.routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(cors());
 
-app.use('/api', UsuarioRutas, HotelRutas, HabitacionRutas, EventoRutas, HospedadosRutas);
+app.use('/api', UsuarioRutas, HotelRutas, HabitacionRutas, EventoRutas,ReservacionRutas ,HospedadosRutas);
 
 
 module.exports = app;
--- src/controllers/reservacion.controller.js
+++ src/controllers/reservacion.controller.js
@@ -1,10 +1,53 @@
-const Reservacion = require('../models/reservacion.model')
+const Reservacion = require('../models/reservacion.model');
+const Habitacion = require('../models/habitaciones.model')
 
-function agregarReservacion(req, res){
+function agregarReservacion(req, res) {
     const parametros = req.body;
     const idUsuario = req.user.sub;
     const idHabitacion = req.params.idHabitacion;
+    const modelReservacion = new Reservacion();
 
-    Reservacion
+    if (idUsuario != null) {
+        Habitacion.findOne({ _id: idHabitacion }, (err, habitacionEncontrada) => {
+            modelReservacion.idUsuario = idUsuario;
+            modelReservacion.tipoDehabitacion = habitacionEncontrada.tipo;
+            modelReservacion.cantidadDias = parametros.cantidadDias;
+            modelReservacion.total = parametros.cantidadDias * habitacionEncontrada.precio;
+            modelReservacion.idHotel = habitacionEncontrada.idHotel;
 
+            modelReservacion.save((err, reservacionGuardada) => {
+                if (err) return res.status(500).send({ mensaje: 'Hubo un error en la peticion' })
+                if (!reservacionGuardada) return res.status(404).send({ mensaje: "Hubo un error al agregar la reservacion" })
+                return res.status(200).send({ reservacion: reservacionGuardada })
+            })
+        })
+    } else {
+        return res.status(500).send({ mensaje: "Tiene que estar logeado para poder reservar un habitacion" })
+    }
+
+}
+function obtenerReservaciones(req, res) {
+    const idHotel = req.user.sub;
+
+    Reservacion.find({ idHotel: idHotel }, (err, reservacionesEncontradas) => {
+        if (err) return res.status(500).send({ mensaje: "Hubo un error en la peticion" })
+        if (!reservacionesEncontradas) return res.status(404).send({ mensaje: "Hubo un error al obtener las reservaciones" })
+        return res.status(200).send({ reservacion: reservacionesEncontradas })
+    
+    })
+
+}
+function obtenerReservacionId(req, res){
+    const idUsuario = req.user.sub; 
+
+    Reservacion.find({ idUsuario: idUsuario}, (err, reservacionObtenida)=>{
+        if(err) return res.status(500).send({mensaje: 'Hubo un error en la peticion'})
+        if(!reservacionObtenida) return res.status(500).send({mensaje: 'Hubo un error al obtener la reservacion'})
+        return res.status(200).send({reservacion: reservacionObtenida})
+    })
+}
+module.exports = {
+    agregarReservacion,
+    obtenerReservacionId,
+    obtenerReservaciones
 }
